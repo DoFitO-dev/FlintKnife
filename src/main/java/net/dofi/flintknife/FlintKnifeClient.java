e9ffe3ff4007fcf5c7bf62c08a007a1bbecc073b
DoFitO-dev <hifiwifi144@gmail.com>
2024-08-10 10:48:40 +0530

    Initial commit

---
+++ src/main/java/net/dofi/flintknife/FlintKnifeClient.java
@@ -0,0 +1,10 @@
+package net.dofi.flintknife;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FlintKnifeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
