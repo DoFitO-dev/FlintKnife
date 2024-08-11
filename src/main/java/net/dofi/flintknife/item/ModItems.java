625ad3a59f622ac9f4a0b0f17b6288d7dc6ebce1
DoFitO-dev <hifiwifi144@gmail.com>
2024-08-11 13:24:35 +0530

    test4

--- src/main/java/net/dofi/flintknife/item/ModItems.java
+++ src/main/java/net/dofi/flintknife/item/ModItems.java
@@ -10,14 +10,23 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-    public static final Item FLINT_KNIFE = registerItem(new Item(new Item.Settings()));
+    public static final Item FLINT_KNIFE = registerItem("flint_knife", new Item(new Item.Settings()));
+    public static final Item FLINT_AXE = registerItem("flint_axe", new Item(new Item.Settings()));
+    public static final Item FLINT_PICKAXE = registerItem("flint_pickaxe", new Item(new Item.Settings()));
+    public static final Item FLINT_SHOVEL = registerItem("flint_shovel", new Item(new Item.Settings()));
+    public static final Item FLINT_HOE = registerItem("flint_hoe", new Item(new Item.Settings()));
 
     private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
         entries.add(FLINT_KNIFE);
+        entries.add(FLINT_AXE);
+        entries.add(FLINT_PICKAXE);
+        entries.add(FLINT_SHOVEL);
+        entries.add(FLINT_HOE);
     }
 
-    private static Item registerItem(Item item) {
-        return Registry.register(Registries.ITEM, Identifier.of(FlintKnife.MOD_ID, "flint_knife"), item);
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(FlintKnife.MOD_ID, name), item);
+
     }
 
     public static void registerModItems() {
